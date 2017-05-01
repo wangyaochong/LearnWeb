fa9a4b27483808029095cc446495f7c04e1ff037
wangyaochong <1162025261@qq.com>
2017-05-01 14:04:42 +0800

    每日算法学习

---
+++ src/main/java/test/TestMath.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Created by【王耀冲】on 【2017/4/28】 at 【21:03】.
+ */
+public class TestMath {
+    public static void main(String[] args) {
+        System.out.println(Math.round(-11.5));
+        System.out.println(Math.round(11.5));
+    }
+}
