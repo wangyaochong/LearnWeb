ef903d7607d0a2c8359ed83aca302e116537e80b
wangyaochong <1162025261@qq.com>
2017-08-08 23:12:47 +0800

    测试requestParam

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -1,7 +1,9 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 @Controller
@@ -12,6 +14,14 @@ public class TestController {
         System.out.println(httpRequest);
         return "test ok";
     }
-
-
+    @RequestMapping("/testParam")
+    @ResponseBody
+    public String testParam(@RequestParam("integer") Integer integerParam){
+        return "integer value: "+Integer.toString(integerParam);
+    }
+    @RequestMapping("/testPathParam/{integer}")
+    @ResponseBody
+    public String testPathParam(@PathVariable("integer") Integer integerParam){
+        return "integer value: "+Integer.toString(integerParam);
+    }
 }
